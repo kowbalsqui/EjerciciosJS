4639cb49869112f146d2ab4e6537f9e70f4f29c4
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2024-11-22 12:54:56 +0100

    termninar

--- Boletin5/Ejercicio5/js/sms.js
+++ Boletin5/Ejercicio5/js/sms.js
@@ -1,9 +1,29 @@
-const campoTexto = document.getElementById("mensaje");
-const contador = document.getElementById("contador");
+window.addEventListener("load", inizializar);
 
-campoTexto.addEventListener("input", () => {
-    const maxLength = campoTexto.maxLength; // Máximo permitido
-    const currentLength = campoTexto.value.length; // Actual
-    const remaining = maxLength - currentLength; // Restantes
-    contador.textContent = `${remaining} caracteres disponibles`;
-});
+
+function inizializar() {
+    const campoTexto = document.getElementById("mensaje");
+    const contador = document.getElementById("contador");
+    const aviso = document.getElementById("aviso");
+    const regex = /^[a-zA-Z0-9.,:;!?()¿¡\-_\s]+$/;
+
+    
+    campoTexto.addEventListener("input", () => {
+        if (campoTexto === ""){
+            document.getElementById("buttom").disabled = true;
+        }else{
+            document.getElementById("buttom").removeAttribute("disabled");
+            const maxLength = campoTexto.maxLength; // Máximo permitido
+            const currentLength = campoTexto.value.length; // Actual
+            const remaining = maxLength - currentLength; // Restantes
+            if(!regex.test(campoTexto.value)){
+                document.getElementById("buttom").disabled = true;
+                aviso.textContent = "Hay un caracter no permitido";
+            }else {
+                contador.textContent = `${remaining} caracteres disponibles`;
+                aviso.textContent = "";
+            }
+        }
+    });
+};
+
