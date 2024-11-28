2f1ce4595f5d74c27314251de64ae06f3b10c3b1
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2024-11-28 14:37:12 +0100

    terminar

---
+++ Boletin5/cockies/eercicio1/js/js.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", inicializar);
+
+function inicializar(){
+    cafe();  // Llama a la función que saluda y crea la cookie
+
+    //Function para crear una cookie.
+    function crearCockie (nombre ,valor, diasExp){
+        const fecha = new Date();
+        fecha.setTime(fecha.getTime() + (diasExp * 24 * 60 * 60 * 1000));  // Sumar los días de expiración
+        const expiracion = "expira = " + fecha.toUTCString(); 
+        document.cookie =  nombre + "=" + valor + ";" + expiracion + ";path=/";
+    }
+
+    //Function para obtener el valor de una cookie.
+
+    function obtenerValorCookie (){
+        return document.cookie
+        .split(';')
+        .find((cookie) => cookie.startsWith("name="))
+        ?.split("=")[1];
+    }
+
+    //Funcion que saluda si tieines cookie y si no te la crea
+
+    function cafe (){
+        const nombre = obtenerValorCookie();
+        if (nombre == ""){
+            document.getElementById('usuario').textContent = `Bienvenido de nuevo ${nombre}`
+        }else{
+            let nombreUser = prompt("Ingresa su nombre");
+            crearCockie("name", `${nombreUser}`, 30);  // Crear cookie con nombre y 30 días de expiración
+        }
+    }
+
+    const boton = document.getElementById('boton');
+    boton.addEventListener('click', ()=>{
+        cafe();
+    });
+}
